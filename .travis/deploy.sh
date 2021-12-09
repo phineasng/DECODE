ba3b970d0f76ae99ac3b19ff8293b5ffff029098
An-phi Nguyen <UYE@zurich.ibm.com>
2021-12-09 12:06:07 +0100

    Initial commit

---
+++ .travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# some steps to (build and) upload your built artifacts from travis.
+# this might include steps to
+# - install cli tools to interact with services (e.g. ibmcloud, openshift, ...)
+# - log in to the registy
+# - tag image/images accordingly for pushing them
+# - actually pushing/uploading the images
+# - trigger other events now that these images are available
