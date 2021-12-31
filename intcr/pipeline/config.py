ffad342c182ff3f2da3c2c4ccac0757218d5501c
uye <uye@zurich.ibm.com>
2021-12-31 15:11:23 +0100

    feat: done first version of pipeline

--- intcr/pipeline/config.py
+++ intcr/pipeline/config.py
@@ -1,1 +1,6 @@
 EXPERIMENT_ROOT_KEY = 'root'
+
+
+def simple_key_check(config, key):
+    if key not in config:
+        raise KeyError('The key "{}" should be present'.format(key))
--- intcr/pipeline/environment.py
+++ intcr/pipeline/environment.py
@@ -1,11 +1,17 @@
 import os
 import warnings
-import pickle
+from intcr.pipeline.utils import load_data, save_data
 
 # constants
 # - step 0. Data Preparation
 PREPARATION_SUBDIR = 'data_preparation'
 SPLIT_FNAME = 'data_split.pkl'
+# - step 1. Clustering
+CLUSTERING_SUBDIR = 'clustering'
+PREPROCESSING_SUBDIR = 'clustering_preprocessing'
+CLUSTERING_RESULTS_SUBDIR = 'clustering_results'
+
+EXPLAINER_SUBDIR = 'explanations'
 
 
 def setup_folder(root_path):
@@ -14,6 +20,7 @@ def setup_folder(root_path):
     os.makedirs(root_path, exist_ok=True)
 
 
+# Preparation step (0)
 def setup_preparation_root(root):
     path = os.path.join(root, PREPARATION_SUBDIR)
     setup_folder(path)
@@ -23,14 +30,34 @@ def setup_preparation_root(root):
 
 def retrieve_prepared_data(preparation_root):
     split_path = os.path.join(preparation_root, SPLIT_FNAME)
-    with open(split_path, 'rb') as split_file:
-        return pickle.load(split_file)
+    return load_data(split_path)
 
 
 def save_prepared_data(preparation_root, split_data):
     split_path = os.path.join(preparation_root, SPLIT_FNAME)
-    with open(split_path, 'wb') as split_file:
-        return pickle.dump(split_data, split_file)
+    save_data(split_path, split_data)
+
+
+# Clustering step (1)
+def setup_clustering_root(root):
+    path = os.path.join(root, CLUSTERING_SUBDIR)
+    setup_folder(path)
+
+    preproc_subdir = os.path.join(path, PREPROCESSING_SUBDIR)
+    setup_folder(preproc_subdir)
+
+    results_subdir = os.path.join(path, CLUSTERING_RESULTS_SUBDIR)
+    setup_folder(results_subdir)
+
+    return preproc_subdir, results_subdir
+
+
+def setup_explainer_root(root):
+    path = os.path.join(root, EXPLAINER_SUBDIR)
+    setup_folder(path)
+    return path
+
+
 
 
 
