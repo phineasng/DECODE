5fa4ebc953aad9e1f181683347f352a031efee0a
uye <uye@zurich.ibm.com>
2021-12-31 15:12:43 +0100

    feat: pipeline utils

---
+++ intcr/pipeline/utils.py
@@ -0,0 +1,31 @@
+import os, warnings
+from joblib import dump, load
+
+
+def load_data(fpath):
+    with open(fpath, 'rb') as in_file:
+        return load(in_file)
+
+
+def save_data(fpath, data):
+    with open(fpath, 'wb') as out_file:
+        dump(data, out_file)
+
+
+def _retrieve_input(input_folder, input_type):
+    input_path = os.path.join(input_folder, input_type)
+    if os.path.exists(input_path):
+        return load_data(input_path)
+    else:
+        raise RuntimeError('{} not found. Are you sure you correctly ran the pre-clustering?')
+
+
+def retrieve_input(config, folder, dict_key, split_samples):
+    if dict_key not in config:
+        warnings.warn('Input type not detected. The original inputs will be passed to the algo.')
+        inputs = split_samples
+        input_type = 'original'
+    else:
+        input_type = config[dict_key]
+        inputs = _retrieve_input(folder, input_type)
+    return inputs, input_type
