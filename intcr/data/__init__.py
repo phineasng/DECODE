428b9ac3bf5d60e9ffe2b9de5d1fe66365b5e135
uye <uye@zurich.ibm.com>
2022-01-02 12:09:09 +0100

    wip: debugging dataset loading

--- intcr/data/__init__.py
+++ intcr/data/__init__.py
@@ -1,7 +1,8 @@
 from intcr.data.tcr_titan import get_aa_tcr_dataset
 
 
-DATASET_KEY = 'dataset'
+DATASET_CONFIG_KEY = 'dataset'
+DATASET_KEY = 'dataset_id'
 DATASET_PARAMS_KEY = 'dataset_params'
 DATASET_GETTERS = {
     'tcr_affinity_smile': get_aa_tcr_dataset,
