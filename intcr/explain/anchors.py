47636333ea9adbe8ea1a531d3e1db5be0729bb4a
uye <uye@zurich.ibm.com>
2022-01-05 00:40:09 +0100

    fix: debugging explainability

--- intcr/explain/anchors.py
+++ intcr/explain/anchors.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from alibi.explainers import AnchorTabular
+from collections import defaultdict
 from intcr.pipeline.utils import retrieve_input, load_data, save_data
 
 
@@ -11,38 +12,55 @@ ANCHORS_INPUT_KEY = 'input_type'
 
 def generate_anchors(assignments, cluster_centers, best_clustering, config, preprocessing_folder, results_root,
                      model, dataset, split_samples, recompute=False, random_seed=None):
+    # prepare explainer
     constructor_params = config.get(ANCHORS_CONSTRUCTOR_PARAMS_KEY, {})
     explanation_params = config.get(ANCHORS_EXPLANATION_PARAMS_KEY, {})
     n_features = len(dataset[0])
     feature_names = ['{}'.format(i) for i in range(n_features)]
-
-    inputs, _ = retrieve_input(config, preprocessing_folder, ANCHORS_INPUT_KEY, split_samples)
-    anchors = dict()
+    categorical_names = {
+        '{}'.format(i): np.unique(dataset[:, i]) for i in range(n_features)
+    }
     anchors_explainer = AnchorTabular(
         predictor=model.predict,
         feature_names=feature_names,
+        categorical_names=categorical_names,
         **constructor_params
     ).fit(dataset[:])
 
+    anchors = defaultdict(dict)
     anchors_fpath = os.path.join(results_root, 'anchors.pkl')
 
     if not os.path.exists(anchors_fpath) or recompute:
         np.random.seed(random_seed)
-        for split, labels in assignments[best_clustering].items():
-            if best_clustering not in cluster_centers:
-                idx = np.arange(len(assignments))
-                centers = []
+        # prepare centers
+        centers = dict()
+        for split in assignments.keys():
+            if best_clustering[split]['method'] not in cluster_centers:
+                curr_centers = []
+                labels = assignments[best_clustering[split]['method']][split]
+                idx = np.arange(len(labels))
                 for i in np.unique(labels):
-                    centers.append(np.random.choice(idx[labels == i]))
-                centers = np.array(centers)
+                    curr_centers.append(np.random.choice(idx[labels == i]))
+                centers[split] = np.array(curr_centers)
             else:
-                centers = cluster_centers[best_clustering][split]
+                centers = cluster_centers[best_clustering[split]['method']][split]
 
-            samples = inputs[split]
-            explanations = []
-            for center in centers:
-                explanations.append(anchors_explainer.explain(samples[center], **explanation_params))
-            anchors[split] = explanations
+        def generate_split_center_args():
+            for sp, cntrs in centers.items():
+                for cntr in cntrs:
+                    yield sp, cntr
+
+        def parallelizable_fn(spl, cnt):
+            inputs, _ = retrieve_input(config, preprocessing_folder, ANCHORS_INPUT_KEY, split_samples, spl)
+            return {spl: {cnt: anchors_explainer.explain(inputs[cnt], **explanation_params)}}
+
+        results = []
+        for s, c in generate_split_center_args():
+            results.append(parallelizable_fn(s, c))
+
+        for r in results:
+            for s, c_exp in r.items():
+                anchors[s].update(c_exp)
         save_data(anchors_fpath, anchors)
     else:
         anchors = load_data(anchors_fpath)
