bfffa463d32d8a818d4cb4130e9eab47a9b142a3
uye <uye@zurich.ibm.com>
2022-03-10 11:16:19 +0100

    feat: added data loading routine for pmtnet data

--- intcr/data/__init__.py
+++ intcr/data/__init__.py
@@ -1,4 +1,5 @@
 from intcr.data.tcr_titan import get_aa_tcr_dataset, BLOSUM_IDX2KEY
+from intcr.data.tcr_pmtnet import get_pmtnet_cdr3_only_dataset
 
 
 DATASET_CONFIG_KEY = 'dataset'
@@ -6,6 +7,7 @@ DATASET_KEY = 'dataset_id'
 DATASET_PARAMS_KEY = 'dataset_params'
 DATASET_GETTERS = {
     'tcr_affinity_smile': get_aa_tcr_dataset,
+    'cdr3_only_pmtnet': get_pmtnet_cdr3_only_dataset,
 }
 CATEGORICAL_ALPHABETS = {
     'blosum_categorical': BLOSUM_IDX2KEY
