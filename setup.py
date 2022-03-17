45ff717a5a026e50360a4ade43e4b6d326555289
uye <uye@zurich.ibm.com>
2022-03-17 11:00:58 +0100

    fix: setup

--- setup.py
+++ setup.py
@@ -26,13 +26,16 @@ def read_version(filepath: str) -> str:
 
 # ease installation during development
 vcs = re.compile(r"(git|svn|hg|bzr)\+")
+VCS_REQUIREMENTS = []
+REQUIREMENTS = []
 try:
     with open("requirements.txt") as fp:
-        VCS_REQUIREMENTS = [
-            str(requirement)
-            for requirement in parse_requirements(fp)
-            if vcs.search(str(requirement))
-        ]
+        for requirement in parse_requirements(fp):
+            requirement = str(requirement)
+            if vcs.search(requirement):
+                VCS_REQUIREMENTS.append(requirement)
+            else:
+                REQUIREMENTS.append(requirement)
 except FileNotFoundError:
     # requires verbose flags to show
     print("requirements.txt not found.")
@@ -40,17 +43,22 @@ except FileNotFoundError:
 
 # TODO: Update these values according to the name of the module.
 setup(
-    name="interpret_tcr_binding",
-    version=read_version("intcr/__init__.py"),  # single place for version
-    description="Globalizing Anchor explanation for TCRs via clustering",
+    name="FLAN",
+    version=read_version("flan/__init__.py"),  # single place for version
+    description="Installable package for FLAN-related projects.",
     long_description=open("README.md").read(),
-    url="https://github.ibm.com/SysBio/interpret_tcr",
-    author="An-phi Nguyen, Iliana Papadopoulou, Anna Maria Weber",
-    author_email="uye@zurich.ibm.com, ipa@zurich.ibm.com, wbr@zurich.ibm.com",
+    url="https://github.ibm.com/SysBio/flan_missing_values",
+    author="Nicolas Deutschmann, An-phi Nguyen",
+    author_email="deu@zurich.ibm.com, uye@zurich.ibm.com",
     # the following exclusion is to prevent shipping of tests.
     # if you do include them, add pytest to the required packages.
     packages=find_packages(".", exclude=["*tests*"]),
-    scripts=["bin/run_interpretability_pipeline"],
+    package_data={"flans": ["py.typed"]},
+    #entry_points="""
+    #    [console_scripts]
+    #    salutation=blueprint.complex_module.core:formal_introduction
+    #""",
+    scripts=["bin/run_experiment.py"],
     extras_require={
         "vcs": VCS_REQUIREMENTS,
         "test": ["pytest", "pytest-cov"],
@@ -71,9 +79,5 @@ setup(
             "myst-parser",
         ],
     },
-    install_requires=[
-        # versions should be very loose here, just exclude unsuitable versions
-        # because your dependencies also have dependencies and so on ...
-        # being too strict here will make dependency resolution harder
-    ],
+    install_requires=REQUIREMENTS,
 )
